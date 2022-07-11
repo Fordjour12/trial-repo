71f6d647d7508c97059b28a9bf361f13bea2e4f7
ThePhantomDev <fordjourbobiem@gmail.com>
2022-07-11 16:50:10 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+app.get("/", (request, response) => {
+	console.log("hello world received a request");
+
+	const target = process.env.Target || "World";
+	response.send(`Hello ${target}`);
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+	console.log("Hello World listening on port", port);
+});
